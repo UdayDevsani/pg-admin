627a6d34cc0ab8bea749fc88eb60d05091e7dd07
Uday Devsani <udaydevsani24@gmail.com>
2024-07-18 14:25:08 +0530

    Stage Changes 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MainContentComponent } from './layouts/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
